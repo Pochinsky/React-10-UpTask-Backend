e8313febcb7edc10788481f3801b950e3d9aa15e
Pochinsky <tomas.guttman@sansano.usm.cl>
2025-07-09 21:27:26 -0400

    ✨ tasks added

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,75 @@
+import type { Request, Response } from "express";
+import Task from "../models/Task";
+
+export class TaskController {
+  static createTask = async (req: Request, res: Response) => {
+    try {
+      const task = new Task(req.body);
+      task.project = req.project.id;
+      req.project.tasks.push(task.id);
+      await Promise.allSettled([task.save(), req.project.save()]);
+      res.send("La tarea ha sido creada");
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error" });
+    }
+  };
+
+  static getProjectTasks = async (req: Request, res: Response) => {
+    try {
+      const tasks = await Task.find({ project: req.project.id }).populate(
+        "project"
+      );
+      res.json(tasks);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error" });
+    }
+  };
+
+  static getTaskById = async (req: Request, res: Response) => {
+    try {
+      res.json(req.task);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error" });
+    }
+  };
+
+  static updateTask = async (req: Request, res: Response) => {
+    try {
+      req.task.name = req.body.name;
+      req.task.description = req.body.description;
+      await req.task.save();
+      res.send("La tarea se ha actualizado");
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error" });
+    }
+  };
+
+  static deleteTask = async (req: Request, res: Response) => {
+    try {
+      req.project.tasks = req.project.tasks.filter(
+        (task) => task._id.toString() !== req.params.taskId
+      );
+      await Promise.allSettled([req.task.deleteOne(), req.project.save()]);
+      res.send("La tarea se ha eliminado");
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error" });
+    }
+  };
+
+  static updateStatus = async (req: Request, res: Response) => {
+    try {
+      const { status } = req.body;
+      req.task.status = status;
+      await req.task.save();
+      res.send("El estado de la tarea se ha actualizado");
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Ocurrió un error" });
+    }
+  };
+}
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { TaskController } from "../controllers/TaskController";
 import { handleInputErrors } from "../middlewares/validation";
+import { projectExists } from "../middlewares/project";
+import { taskBelongsToProject, taskExists } from "../middlewares/task";
 
 const router = Router();
+
+/** Routes for projects */
 
 router.post(
   "/",
@@ -50,6 +55,58 @@ router.delete(
   param("id").isMongoId().withMessage("El ID no es válido"),
   handleInputErrors,
   ProjectController.deleteProject
+);
+
+/** Routes for tasks */
+
+router.param("projectId", projectExists);
+
+router.post(
+  "/:projectId/tasks",
+  body("name").notEmpty().withMessage("El nombre de la tarea es obligatoria"),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria"),
+  handleInputErrors,
+  TaskController.createTask
+);
+
+router.get("/:projectId/tasks", TaskController.getProjectTasks);
+
+router.param("taskId", taskExists);
+router.param("taskId", taskBelongsToProject);
+
+router.get(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("El ID no es válido"),
+  handleInputErrors,
+  TaskController.getTaskById
+);
+
+router.put(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("El ID no es válido"),
+  body("name").notEmpty().withMessage("El nombre de la tarea es obligatoria"),
+  body("description")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria"),
+  handleInputErrors,
+  TaskController.updateTask
+);
+
+router.delete(
+  "/:projectId/tasks/:taskId",
+  param("taskId").isMongoId().withMessage("El ID no es válido"),
+  handleInputErrors,
+  TaskController.deleteTask
+);
+
+router.post(
+  "/:projectId/tasks/:taskId/status",
+  param("taskId").isMongoId().withMessage("El ID no es válido"),
+  body("status").notEmpty().withMessage("El estado de tarea es obligatorio"),
+  handleInputErrors,
+  TaskController.updateStatus
 );
 
 export default router;
