e0a1c174e7f6aad45fc405e2577ba7703a17be31
Pochinsky <tomas.guttman@sansano.usm.cl>
2025-08-01 01:31:58 -0400

    🛂 add frontend to CORS

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 
 connectDB();
 
 const server = express();
+
+server.use(cors(corsConfig));
 
 // Lecture JSON
 server.use(express.json());
